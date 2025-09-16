d3f7807eafd9100e319684ac3a999079dac94931
Akira <37931@ma-web.nl>
2025-09-16 15:38:53 +0200

    commit

---
+++ public/cv.php
@@ -0,0 +1,20 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Portfolio</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
+  <nav>
+    <a href="index.php">Home</a>
+    <a href="projects.php">Projects</a>
+    <a href="cv.php" class="active">CV</a>
+    <a href="about.php">About Me</a>
+    <a href="contact.php">Contact</a>
+  </nav>
+</body>
+</html>
